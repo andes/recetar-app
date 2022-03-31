6e784acab5f20f387fbdd527a7f77f06ba286754
martinebucarey <martinebucarey@gmail.com>
2022-03-31 10:25:12 -0300

    feat(user): form new user

---
+++ src/app/auth/components/new-user/new-user.component.ts
@@ -0,0 +1,105 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, AbstractControl, FormGroupDirective } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AuthService } from '@auth/services/auth.service';
+import { ProfessionalsService } from '../../../services/professionals.service';
+import * as moment from 'moment';
+import { Router } from '@angular/router';
+
+@Component({
+    selector: 'app-new-user',
+    templateUrl: './new-user.component.html',
+    styleUrls: ['./new-user.component.sass']
+})
+export class NewUserComponent implements OnInit {
+
+    public newUserForm: FormGroup;
+    public error: string;
+    public roleSelected: 'professional';
+    public regexPassword = '^([a-zA-Z]{8,15})$';
+    public regexEmail = '^[a-z0-9._%+-]+@[a-z0-9.-]+[\.]{1}[a-z]{2,4}$';
+
+    constructor(
+        private fBuilder: FormBuilder,
+        private authService: AuthService,
+        private _snackBar: MatSnackBar,
+        private professionalsService: ProfessionalsService,
+        private router: Router
+    ) { }
+
+    ngOnInit(): void {
+        this.initNewUserForm();
+    }
+
+    initNewUserForm(): void {
+        this.newUserForm = this.fBuilder.group({
+            dni: [''],
+            cuil: [''],
+            username: [''],
+            enrollment: [''],
+            email: ['', Validators.required],
+            businessName: [''],
+            password: ['', Validators.required],
+            roleType: ['', Validators.required]
+        });
+    }
+
+    checkUser() {
+        this.newUserForm.controls.username.setValue(this.newUserForm.get('dni').value ? this.newUserForm.get('dni').value : this.newUserForm.get('email').value);
+    }
+
+    onSubmitEvent(newUserForm: FormGroup, newUserNgForm: FormGroupDirective) {
+        if (this.newUserForm.valid) {
+            this.checkUser();
+            if (this.roleSelected === 'professional') {
+                this.professionalsService.getProfessionalByDni(newUserForm.get('dni').value).subscribe(res => {
+                    if (res.length) {
+                        const profesional = res[0];
+                        const { profesiones } = profesional;
+                        if (this.checkMatricula(profesiones)) {
+                            this.userRegister(newUserForm, newUserNgForm)
+                        } else {
+                            this._snackBar.open('La matricula no es correcta', 'cerrar', {
+                                duration: 5000
+                            });
+                        }
+                    } else {
+                        this._snackBar.open('Profesional no registrado, revise los datos ingresados', 'cerrar', {
+                            duration: 5000
+                        });
+                    }
+                })
+            } else {
+                this.userRegister(newUserForm, newUserNgForm);
+            }
+        }
+    }
+
+    userRegister(newUserForm: FormGroup, newUserNgForm: FormGroupDirective) {
+        this.authService.register(this.newUserForm.value).subscribe
+            (() => {
+                this.cancelar();
+                this._snackBar.open('Usuario creado', 'cerrar', {
+                    duration: 5000
+                });
+                newUserNgForm.resetForm();
+                newUserForm.reset();
+            },
+                err => {
+                    this._snackBar.open(`Eror: ${JSON.stringify(err.error)}'`, 'cerrar', {
+                        duration: 5000
+                    });
+                })
+    }
+
+    checkMatricula(profesiones) {
+        const lastProfesion = profesiones[profesiones.length - 1];
+        const lastMatriculacion = lastProfesion.matriculacion[lastProfesion.matriculacion.length - 1];
+        const res = ((moment(lastMatriculacion.fin)) > moment() && (lastMatriculacion.matriculaNumero).toString() === this.newUserForm.get('enrollment').value);
+        return res;
+    }
+
+    cancelar() {
+        this.router.navigate(['/auth/login']);
+    }
+}
