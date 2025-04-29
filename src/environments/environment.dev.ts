0b1e2d956738737a7283e0378260b12ca5a8cf1a
silviroa <silviroa@gmail.com>
2025-04-29 09:58:23 -0300

    DOC:  Agrega configuracion de entorno DEV

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
-  API_END_POINT: 'https://prescription-validator-api.herokuapp.com/api',
-  ANDES_API: 'https://app.andes.gob.ar/api'
+    production: false,
+    API_END_POINT: 'https://recetardemo.andes.gob.ar/api',
+    ANDES_API: 'https://app.andes.gob.ar/api'
 };
