3ba5ef1699c36a37ea560a922db8023881254948
mcele <celeste.ramos@fi.uncoma.edu.ar>
2025-08-29 16:58:02 -0300

    lint: elimina console.log

--- src/app/auth/token-interceptor.service.ts
+++ src/app/auth/token-interceptor.service.ts
@@ -9,74 +9,72 @@ import { environment } from '@root/environments/environment';
 @Injectable()
 export class TokenInterceptorService implements HttpInterceptor {
 
-  private isRefreshing = false;
-  private refreshTokenSubject: BehaviorSubject<any> = new BehaviorSubject<any>(null);
+    private isRefreshing = false;
+    private refreshTokenSubject: BehaviorSubject<any> = new BehaviorSubject<any>(null);
 
-  constructor(private authService: AuthService, private router: Router ) { }
+    constructor(private authService: AuthService, private router: Router) { }
 
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const urlPublica = request.headers.get('public') === 'true';
-    if (!urlPublica && this.authService.getJwtToken()) {
-      console.log('Adding token to request:');
-      request = this.addToken(request, this.authService.getJwtToken());
-    }
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        const urlPublica = request.headers.get('public') === 'true';
+        if (!urlPublica && this.authService.getJwtToken()) {
+            request = this.addToken(request, this.authService.getJwtToken());
+        }
 
-    return next.handle(request).pipe(catchError(error => {
-      if (error instanceof HttpErrorResponse && error.status === 406) {
-        return this.handle406Error(request, next);
-      } else if (error instanceof HttpErrorResponse && error.status === 417){
-        this.handle417Error(request, next);
-        return throwError(error.error.message);
-      }
-      else {
-        return this.errorHandler(error);
-      }
-    }));
-  }
-
-  errorHandler(err: HttpErrorResponse){
-    if(err.status == 422){
-      return throwError(err);
-    }
-    return throwError(err.error.message || "Server Error");
-  }
-
-  private addToken(request: HttpRequest<any>, token: string) {
-    return request.clone({
-      setHeaders: {
-        'Authorization': `Bearer ${token}`
-      }
-    })
-  }
-
-  // expired token handler
-  private handle406Error(request: HttpRequest<any>, next: HttpHandler) {
-    if (!this.isRefreshing) {
-      this.isRefreshing = true;
-      this.refreshTokenSubject.next(null);
-
-      return this.authService.refreshToken().pipe(
-        switchMap((token: any) => {
-          this.isRefreshing = false;
-          this.refreshTokenSubject.next(token.jwt);
-          return next.handle(this.addToken(request, token.jwt));
+        return next.handle(request).pipe(catchError(error => {
+            if (error instanceof HttpErrorResponse && error.status === 406) {
+                return this.handle406Error(request, next);
+            } else if (error instanceof HttpErrorResponse && error.status === 417) {
+                this.handle417Error(request, next);
+                return throwError(error.error.message);
+            } else {
+                return this.errorHandler(error);
+            }
         }));
-
-    } else {
-      return this.refreshTokenSubject.pipe(
-        filter(token => token != null),
-        take(1),
-        switchMap(jwt => {
-        return next.handle(this.addToken(request, jwt));
-      }));
     }
-  }
 
-  // user credentials handler
-  private handle417Error(request: HttpRequest<any>, next: HttpHandler) {
-    return this.authService.logout().subscribe(success => {
-      if(success) this.router.navigate(['/auth/login']);
-    });
-  }
+    errorHandler(err: HttpErrorResponse) {
+        if (err.status == 422) {
+            return throwError(err);
+        }
+        return throwError(err.error.message || 'Server Error');
+    }
+
+    private addToken(request: HttpRequest<any>, token: string) {
+        return request.clone({
+            setHeaders: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+    }
+
+    // expired token handler
+    private handle406Error(request: HttpRequest<any>, next: HttpHandler) {
+        if (!this.isRefreshing) {
+            this.isRefreshing = true;
+            this.refreshTokenSubject.next(null);
+
+            return this.authService.refreshToken().pipe(
+                switchMap((token: any) => {
+                    this.isRefreshing = false;
+                    this.refreshTokenSubject.next(token.jwt);
+                    return next.handle(this.addToken(request, token.jwt));
+                }));
+
+        } else {
+            return this.refreshTokenSubject.pipe(
+                filter(token => token != null),
+                take(1),
+                switchMap(jwt => {
+                    return next.handle(this.addToken(request, jwt));
+                }));
+        }
+    }
+
+    // user credentials handler
+    private handle417Error(request: HttpRequest<any>, next: HttpHandler) {
+        return this.authService.logout().subscribe(success => {
+            if (success) { this.router.navigate(['/auth/login']); }
+        });
+    }
 
 }
