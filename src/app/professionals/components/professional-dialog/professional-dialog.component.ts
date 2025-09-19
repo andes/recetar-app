95055b64336a4d8f03354b97533d32fcd4d8fe8f
AgustinRodriguez-Andes <63318331+agustin1996ra@users.noreply.github.com>
2025-09-19 17:32:06 -0300

    FIx: suspender recetas publicas

--- src/app/professionals/components/professional-dialog/professional-dialog.component.ts
+++ src/app/professionals/components/professional-dialog/professional-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, OnInit, } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Prescriptions } from '@interfaces/prescriptions';
+import AndesPrescriptions from '@interfaces/andesPrescriptions';
 import { InteractionService } from '@professionals/interaction.service';
 import { Certificate } from '@interfaces/certificate';
 import { fadeInOnEnterAnimation, fadeOutOnLeaveAnimation } from 'angular-animations';
@@ -29,26 +30,48 @@ export class ProfessionalDialogComponent implements OnInit {
         this.dialogRef.close();
     }
 
-    deletePrescription(prescription: Prescriptions) {
-        this._interactionService.deletePrescription(prescription).subscribe(
-            success => {
-                if (success) {
-                    // Emitir evento para actualizar listas
-                    this._interactionService.emitPrescriptionDeleted(prescription);
-                    this.dialogRef.close('deleted');
-                } else {
-                    this.dialogRef.close('error');
+    deletePrescription(prescription: Prescriptions | AndesPrescriptions) {
+        // Solo procesar si es una prescripción local
+        if (!this.isAndesPrescription(prescription)) {
+            const localPrescription = prescription as Prescriptions;
+            this._interactionService.deletePrescription(localPrescription).subscribe(
+                success => {
+                    if (success) {
+                        // Emitir evento para actualizar listas
+                        this._interactionService.emitPrescriptionDeleted(localPrescription);
+                        this.dialogRef.close('deleted');
+                    } else {
+                        this.dialogRef.close('error');
+                    }
+                },
+                error => {
+                    this.dialogRef.close('error-dispensed');
                 }
-            },
-            error => {
-                this.dialogRef.close('error-dispensed');
-            }
-        );
+            );
+        }
+    }
+
+    confirmSuspendAndes() {
+        this.dialogRef.close('suspend_andes');
+    }
+
+    isAndesPrescription(item: any): item is AndesPrescriptions {
+        return 'idAndes' in item || 'paciente' in item;
+    }
+
+    getPatientName(): string {
+        if (this.isAndesPrescription(this.data.item)) {
+            const andesPrescription = this.data.item as AndesPrescriptions;
+            return `${andesPrescription.paciente?.apellido}, ${andesPrescription.paciente?.nombre}`;
+        } else {
+            const localPrescription = this.data.item as Prescriptions;
+            return `${localPrescription.patient?.lastName}, ${localPrescription.patient?.firstName}`;
+        }
     }
 }
 
 export interface DialogData {
-    item: Prescriptions;
+    item: Prescriptions | AndesPrescriptions;
     dialogType: string;
     text: string;
 }
