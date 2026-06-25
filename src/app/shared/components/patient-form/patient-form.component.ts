0ca12b23b011b896b9387c1b6b90a2a419bf7788
ma7payne <matich04@gmail.com>
2026-06-25 11:40:59 -0300

    feat(REC): agrega organizaciones al crear certificados

--- src/app/shared/components/patient-form/patient-form.component.ts
+++ src/app/shared/components/patient-form/patient-form.component.ts
@@ -174,6 +174,18 @@ export class PatientFormComponent implements OnInit, OnDestroy, ControlValueAcce
                 }
             });
 
+            // Sincronizar estado disabled de numeroAfiliado con el valor de os.nombre
+            this.patientForm.get('os.nombre')?.valueChanges.subscribe((nombreValue) => {
+                const numeroAfiliado = this.patientForm.get('os.numeroAfiliado');
+                if (numeroAfiliado) {
+                    if (nombreValue) {
+                        numeroAfiliado.enable({ emitEvent: false });
+                    } else {
+                        numeroAfiliado.disable({ emitEvent: false });
+                    }
+                }
+            });
+
             // Configurar autocompletado de obras sociales
             this.filteredObrasSociales = this.patientForm.get('os.nombre')?.valueChanges.pipe(
                 startWith(''),
@@ -210,7 +222,7 @@ export class PatientFormComponent implements OnInit, OnDestroy, ControlValueAcce
             ]],
             lastName: ['', Validators.required],
             firstName: ['', Validators.required],
-            nombreAutopercibido: ['', Validators.required],
+            nombreAutopercibido: [''],
             sex: ['', Validators.required],
             fechaNac: ['', this.showFechaNac ? [
                 Validators.required,
